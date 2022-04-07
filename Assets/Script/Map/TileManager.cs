10e68d3ab98238a0376333ba85b8097f25440924
SoLoHK525 <dickylau2013@gmail.com>
2022-04-07 19:44:27 +0800

    need help

--- Assets/Script/Map/TileManager.cs
+++ Assets/Script/Map/TileManager.cs
@@ -7,11 +7,15 @@ public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
     public float spawnPos = 0;
-    public float tileLength = 30;
+    public Bounds tileBound;
     public int numberOfTiles = 6; //no. of tiles want to be shown on the screen
 
     public Transform character;
+
+    private GameObject initialTile;
+    
     private List<GameObject> activeTiles = new List<GameObject>();
+    
     private List<GameObject> tilePool = new List<GameObject>();
 
     public void Start()
@@ -22,45 +26,54 @@ public class TileManager : MonoBehaviour
 
     private void InstantiateTiles()
     {
+        initialTile = tilePrefabs[0];
+        Instantiate(initialTile);
+        initialTile.SetActive(false);
+        
+        tileBound = new Bounds(Vector3.zero, Vector3.zero);
+        
+        foreach (Transform child in initialTile.transform)
+        {
+            tileBound.Encapsulate(child.GetComponent<BoxCollider>().bounds);
+        }
+
         foreach(var tile in tilePrefabs)
         {
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
                 var t = Instantiate(tile);
                 t.SetActive(false);
                 tilePool.Add(t);
             }
         }
-
+        
         tilePool = ListUtility.Shuffle(tilePool);
+        tilePool.Insert(0, initialTile);
+        
+        Debug.Log($"Bounds {tileBound.size.z}");
     }
 
     private void InitializeTiles()
     {
-        for(int i = 0; i < numberOfTiles; i++)
+        SpawnTile(0);
+        
+        for(int i = 1; i < numberOfTiles; i++)
         {
-            if(i == 0)
-               SpawnTile(0);
-            else
-               SpawnTile(Random.Range(1, tilePool.Count));
+            SpawnTile(Random.Range(0, tilePool.Count));
         }
     }
-
     
     void Update()
     {
-        if(character.position.z - 35 > spawnPos - (numberOfTiles * tileLength))
+        if(character.position.z - 35 > spawnPos - (numberOfTiles * tileBound.size.z))
         {
-            SpawnTile(Random.Range(1, tilePool.Count));
+            SpawnTile(Random.Range(0, tilePool.Count));
             DeleteTile();    // delete the odd tile whenever a new tile has been created
         }
     }
 
     public void SpawnTile(int tileIndex)
     {
-        Debug.Log("SpawnTile " + tileIndex);
-        Debug.Log(tilePool.Count);
-        
         GameObject gameObject = tilePool[tileIndex];
         tilePool.RemoveAt(tileIndex);
         
@@ -70,14 +83,15 @@ public class TileManager : MonoBehaviour
         gameObject.transform.rotation = objectTransform.rotation;
         gameObject.SetActive(true);
 
-        spawnPos += tileLength;
+        spawnPos += tileBound.size.z;
     }
 
     private void DeleteTile()
     {
-        //Destroy(activeTiles[0]);
         GameObject gameObject = activeTiles[0];
-        activeTiles.RemoveAt(0);
+        activeTiles.Remove(gameObject);
+        gameObject.SetActive(false);
         tilePool.Add(gameObject);
+        Debug.Log($"{gameObject.name}");
     }
 }
