cb3240562bccf3895ba1ecdf5e4560d80f76e0e2
Felix Wong <felixwongyuenlam@gmail.com>
2022-04-19 14:07:18 +0800

    Add game countdown

--- Assets/Script/GameManage.cs
+++ Assets/Script/GameManage.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManage : MonoBehaviour
 {
     public GameObject gameOverPanel;
     private AudioManage audioManage;
+
+    [SerializeField] bool isMultiplayer = false;
+
+    [SerializeField] TextMeshProUGUI countDownTxt;
+    [SerializeField] int countDownSeconds = 5;
 
     public event Action onGameStart;
     public event Action onGameOver;
@@ -14,6 +20,32 @@ public class GameManage : MonoBehaviour
     private void Awake()
     {
         audioManage = FindObjectOfType<AudioManage>();
+    }
+
+    private void Start()
+    {
+        if (!isMultiplayer) StartCountDown();
+    }
+
+    private void StartCountDown()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    private IEnumerator CountDown()
+    {
+        int curSec = countDownSeconds;
+        countDownTxt.gameObject.SetActive(true);
+        countDownTxt.text = curSec.ToString();
+        while (curSec > 0)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            curSec--;
+            countDownTxt.text = curSec.ToString();
+        }
+        countDownTxt.gameObject.SetActive(false);
+        audioManage.PlaySound("MainTheme");
+        if (onGameStart != null) onGameStart();
     }
 
     public void EndGame()
