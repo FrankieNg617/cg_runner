f84172074e1435ba445c54a525f14f5ce17e33b1
Felix Wong <felixwongyuenlam@gmail.com>
2022-04-20 16:55:58 +0800

    Styling lobby screen

--- Assets/Script/UI/LobbyUI.cs
+++ Assets/Script/UI/LobbyUI.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class LobbyUI : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI roomNameField;
+
     //  TMP: Only used character names
     //  TODO: Change entire texture character
     [SerializeField] List<GameObject> characters;
@@ -15,6 +20,7 @@ public class LobbyUI : MonoBehaviour
         {
             character.SetActive(false);
         }
+        roomNameField.text = PhotonNetwork.CurrentRoom.Name;
     }
 
     public void AddPlayer(string playerName)
